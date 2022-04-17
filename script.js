1e3e90c3e06928dfb6bfaa9a2a9f27670ed66bc7
AdithyaPrasad7 <89543320+AdithyaPrasad7@users.noreply.github.com>
2022-04-17 21:37:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -284,13 +284,3 @@ var greySquare = function(square) {
     squareEl.css('background', background);
 };
 
-var cfg = {
-    draggable: true,
-    position: 'start',
-    onDragStart: onDragStart,
-    onDrop: onDrop,
-    onMouseoutSquare: onMouseoutSquare,
-    onMouseoverSquare: onMouseoverSquare,
-    onSnapEnd: onSnapEnd
-};
-board = ChessBoard('board', cfg);
